2260646d0a46e3ed2ff569a8c4ae5780839c2686
Knight <42980790+793530@users.noreply.github.com>
2018-10-02 13:08:27 -0700

    commit

--- Labs/Lab904_Attraction/sketch.js
+++ Labs/Lab904_Attraction/sketch.js
@@ -1,21 +1,18 @@
 //  Global variables
 var b1;
 var balls = [];
-var streeringForce
 // put setup code here
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20,20,20);
-  var loc = createVector(random(width), random(height));
+  var loc = createVector(20,20);
   var rad = 25;
   var col = color(0,255,0);
+
+
   b1 = new Ball(loc,rad,col);
   loadBalls(12);
-  function steeringforce(){
-       var steeringForce = p5.Vector.sub(this.loc, other.loc);
-       steeringForce.normalize();  //  changes the magnitud to 1
-       steeringForce.mult(0.5);
 }
 
 function draw() {
@@ -23,6 +20,8 @@ function draw() {
   b1.run();
   for(var i = 0; i<balls.length; i++){
      balls[i].run();
+
+
 }
 }
 
@@ -31,9 +30,7 @@ function loadBalls(numBalls){
       var loc = createVector(random(width), random(height));
       var rad = 15;
       var col = color(255,0,0);
+
       balls.push(new Ball(loc,rad,col))
     }
-
-
-}
 }
