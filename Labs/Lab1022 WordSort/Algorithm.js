9c8856115e437ef9d957eefd7479f75b43efb530
Knight <42980790+793530@users.noreply.github.com>
2018-10-24 08:50:29 -0700

    Commit

--- Labs/Lab1022 WordSort/Algorithm.js
+++
@@ -1,3 +0,0 @@
-// Kalpa Yakupitiyage
-// Lab 1022 WordSort
-// 10/22/2018
---
+++ Labs/Lab1022 WordSort/bubbleSort.js
@@ -0,0 +1,39 @@
+// Kalpa Yakupitiyage
+// Lab 1022 WordSort
+// 10/24/2018
+
+
+var txt;
+
+function preload(){
+  txt = loadStrings(words.txt);
+
+}
+
+function setup() {
+  n = length()
+    for(var i = nums.length - 1; i >= 1; i-- ){
+      for(var j = 0; j < nums.length; j++ ){
+        if 
+  }
+
+
+  }
+
+}
+
+function swap(){
+  var temp = words[a];
+
+
+
+}
+
+
+//for(var i = nums.length - 1; i >= 1; i-- ){
+    //for(var j = 0; j < nums.length; j++ ){
+
+//}
+
+
+//}
