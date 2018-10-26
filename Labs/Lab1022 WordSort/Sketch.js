4135bb514250e7f9884b279f977d1476da1ac4e2
Knight <42980790+793530@users.noreply.github.com>
2018-10-26 13:34:06 -0700

    Commit

--- Labs/Lab1022 WordSort/Sketch.js
+++ Labs/Lab1022 WordSort/Sketch.js
@@ -4,19 +4,38 @@
 
 
 var txt;
-var words;
 
 function preload(){
-  txt = loadStrings(words.txt);
+  txt = loadStrings("words.txt");
 
 }
 
 function setup() {
-  n = length()
-    for(var i = length - 1; i >= 1; i-- ){
-      for(var j = 0; j < nums.length; j++ ){
+
+  console.log(txt)
+  bubbleSort(txt);
+
+
+}
+
+function bubbleSort(txt){
+
+  var length = txt.length
+  for(var i + length-1; i > 0; i--){
+      for(var j = 0; j > i; j++ ){
+
+
+
+
+      }
+
+
 
   }
+
+
+}
+
 
 
 
