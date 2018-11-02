2b10f2f532136ffd980407d132f6174d33502a23
Kalpa Yakupitiyage <42980790+793530@users.noreply.github.com>
2018-11-02 14:08:10 -0700

    commit

--- Labs/1101JSON/sketch.js
+++ Labs/1101JSON/sketch.js
@@ -1,15 +1,53 @@
 //Kalpa Yakupitiyage
 //1101JSON
 //11/01/2018
-
-
 var data;
 
-function preload(){
-  data = loadJSON("rows.json");
+function setup(){
+    loadJSON("onlinePlayers.json", gotData, 'jasonp');
 }
 
-function setup(){
-  data.socialmediadata[-1].fieldName
+function gotData(newData ){
+  console.log(newData);
+  data = newData;
+}
+var json;
+
+function preload(){           //This opens the text file
+  json = loadStrings("worldCup.json");
+
+}
+
+function setup() {
+
+  console.log(json)            //
+  bubbleSort(json);            //
+
+
+}
+
+function bubbleSort(json){               //The code keeps repeating until it is false
+
+  var length = json.length
+  for(var i + length-1; i > 0; i--){
+      for(var j = 0; j > i; j++ ){
+        if(json[j] > json[j+1]){
+          var tmp =txt[j];
+          json[j] = json[j+1];
+          json[j+1 = tmp];
+
+
+        }
+
+
+
+
+
+      }
+
+
+
+  }
+
 
 }
